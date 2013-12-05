05809a8c03b87e67a4c6b0690e7fd0eb8f4ca044
sundayspecial <dobryve4aruhatu@gmail.com>
2013-12-04 23:05:23 -0800

    hw31 added

--- hw25/run.sh
+++ hw25/run.sh
@@ -2,6 +2,6 @@
 
 # This command executes the test
 
-ruby ./lib/static_element_validation.rb -d www.shopping.com -p ./etc/test_specs/sdc_page_urls.json
+bundle exec ruby ./lib/static_element_validation.rb -d www.shopping.com -p ./etc/test_specs/sdc_page_urls.json
 # bundle exec ruby ./lib/static_element_validation.rb -d www.shopping.com -p ./etc/test_specs/sdc_page_urls.json --exitstatus
 
